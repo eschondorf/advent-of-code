42081ed15015fcd1677fa24fb58320606951cc00
eschondorf <emschondorf@gmail.com>
2022-12-05 01:53:19 -0600

    parsed input progamatically

--- 2022/day_5.py
+++ 2022/day_5.py
@@ -3,10 +3,12 @@ import math
 import time
 import copy
 
-def solver(inpt, stack):
+def solver(inpt, stack, rd_inpt = False):
     rv1 = ""
     rv2 = ""
     stack2 = copy.deepcopy(stack)
+    if rd_inpt:
+        pass
     for line in inpt:
         _, num_to_move, _, frm,  _, to = line.split(" ")
         num_to_move = int(num_to_move)
@@ -24,11 +26,35 @@ def solver(inpt, stack):
     return str(rv1), str(rv2)
 
 
+def read_inpt(file):
+    with open(file) as f:
+        txt = f.read()
+        stacks_str, moves_str = txt.split("\n\n")  
+        stacks_lines = stacks_str.split("\n")
+        moves_lines = moves_str.split("\n")
+        cols = stacks_lines[-1]
+        items = stacks_lines[:-1]
+        stack = {}
+        for col in cols:
+            if col.strip() != '':
+                stack[col] = []
+        for elt in reversed(items):
+            row = elt.split(" ")
+            blank = 0
+            for i, x in enumerate(row):
+                if row[i] != '':
+                    stack[str(int(i -(.75*blank) + 1))].append(x[1])
+                else:
+                    blank += 1
+        return stack, moves_lines
+
+
+
 
 if __name__ == "__main__":
     inpt = util.read_strs("inputs/day_5_input.txt", sep = "\n")
     tst = util.read_strs("inputs/day_5_test.txt", sep = "\n")
-    tst = tst[5:]
+    #tst = tst[5:]
     tst_dict = {}
     tst_dict['1']  = ['Z', 'N']
     tst_dict['2']  = ['M', 'C', 'D']
@@ -44,7 +70,11 @@ if __name__ == "__main__":
     inpt_dict['8']  = ['N','G','M','T','B','F', 'Q', 'H']
     inpt_dict['9']  = ['R','D','G','C','P','B', 'Q', 'W']
     print("TASK 1 and 2")
-    util.call_and_print(solver, inpt, inpt_dict)
-    util.call_and_print(solver, tst, tst_dict)
+    util.call_and_print(solver, inpt[10:], inpt_dict)
+    util.call_and_print(solver, tst[5:], tst_dict)
 
+    tst_stack, tst_lines = read_inpt("inputs/day_5_test.txt")
+    inpt_stack, inpt_lines = read_inpt("inputs/day_5_input.txt")
+    util.call_and_print(solver, inpt_lines, inpt_stack, True)
+    util.call_and_print(solver, tst_lines, tst_stack, True)
     
